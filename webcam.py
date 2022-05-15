25c9b413ac3f314dcc7146718da09411bd6cb448
José Luis Luna <pepeluna23@yahoo.com>
2022-05-15 15:08:31 -0500

    Creación ip.txt y modificación webcam.py
    La modificación permite que la url de la ip si no existe no se termite erróneamente el programa.

--- webcam.py
+++ webcam.py
@@ -4,11 +4,31 @@ import cv2
 ip=""
 url=""
 
+def lecturaIp():
+    pass
+
 def saveIp ():
     ip = str(input("Ingrese la ip a usar:"))
     url = "http:/"+ip+":8080/video"
     return url
  
-url=saveIp()
+url = saveIp()
 print(url)
+
+
+try:
     
+    capture = cv2.VideoCapture(url)
+
+    while(True):
+        _, frame = capture.read()
+        cv2.imshow('videoc', frame)
+        
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+except Exception:
+    print("Error: url incorrecta")
